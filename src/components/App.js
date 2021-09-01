792d649f213e546eaa3b696c38fdb85675c87ed9
MBEDOBE <danielbushbaby@gmail.com>
2021-09-01 10:01:37 +0000

    major updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import { LoadingBar } from "react-redux-loading";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
   render() {
     return (
       <div>
-        Starter Code
+        <LoadingBar />
+        {this.props.loading === true ? null : <Dashboard />}
       </div>
-    )
+    );
   }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+export default connect(mapStateToProps)(App);
